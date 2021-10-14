d566fae9210a2cb58cea1de3086b46114452ab79
alexander <posb00@gmail.com>
2021-10-14 14:00:56 -0400

    migrations create achievement table

---
+++ database/migrations/2021_10_12_160945_create_achievements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAchievementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('value');
+            $table->foreignId('achievements_type_id')->constrained();
+            $table->integer('next')->nullable();
+            $table->unique(['name', 'achievements_type_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievements');
+    }
+}
