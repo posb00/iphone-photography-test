7aa3810355536c58b6e609dd69fc304146a70b48
alexander <posb00@gmail.com>
2021-10-14 14:12:58 -0400

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Achievement;
 use App\Models\Comment;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -63,5 +64,129 @@ class User extends Authenticatable
     public function watched()
     {
         return $this->belongsToMany(Lesson::class)->wherePivot('watched', true);
+    }
+
+    /**
+     * The badges that a user has earned.
+     */
+    public function badges()
+    {
+        return $this->belongsToMany(Badge::class);
+    }
+
+    /**
+     * The badges that a user has earned.
+     */
+    public function currentBadge()
+    {
+        return $this->belongsToMany(Badge::class)->orderBy('value', 'desc');
+    }
+
+    /**
+     * The achievements that a user has.
+     */
+    public function achievements()
+    {
+        return $this->belongsToMany(Achievement::class);
+    }
+
+    /**
+     * Return users next badges.
+     */
+
+    public function getNextBadgeAttribute()
+    {
+        $currentBadgeValue = $this->currentBadge()
+            ->pluck('value')
+            ->first();
+
+        return Badge::where('value', '>', $currentBadgeValue)
+            ->orderBy('value', 'asc')
+            ->pluck('name')
+            ->first();
+
+    }
+
+    /**
+     * Return users current badge.
+     */
+
+    public function getCurrentBadgeAttribute()
+    {
+        return $this->currentBadge()
+            ->pluck('name')
+            ->first();
+
+    }
+
+    /**
+     * Return users remainging badges.
+     */
+
+    public function getRemainingNextBadgeAttribute()
+    {
+        $currentBadgeValue = $this->achievements()
+            ->count();
+
+        $nextBadge = Badge::where('value', '>', $currentBadgeValue)
+            ->orderBy('value', 'asc')
+            ->pluck('value')
+            ->first();
+
+        $total = $nextBadge - $currentBadgeValue;
+
+        return $total < 0 ? 0 : $total;
+
+    }
+
+    /**
+     * Return users unlocked achievements.
+     */
+
+    public function getUnlockedAchievementAttribute()
+    {
+        return $this->achievements()->pluck('name');
+
+    }
+
+    /**
+     * Return users next available achievements.
+     */
+
+    public function getnextAvailableAchievementsAttribute()
+    {
+        $nextAchievements = [];
+
+        //get list of all achievements for only make one query
+
+        $achievementList = collect(Achievement::all());
+
+        //get list of achievements with id and value
+
+        $achievements = Achievement::whereIn('id', $this->achievements()->pluck('id'))
+            ->groupBy('achievements_type_id')
+            ->selectRaw('achievements_type_id as type, max(value) as value')
+            ->get();
+
+        //get next value achievement name
+
+        foreach ($achievements as $achievement) {
+
+            $name = $achievementList->where('achievements_type_id', $achievement->type)
+                ->where('value', '>', $achievement->value)
+                ->sortBy('value')
+                ->pluck('name')
+                ->first();
+
+            //if name is null dont push to array
+
+            if ($name) {
+                $nextAchievements[] = $name;
+            }
+
+        }
+
+        return $nextAchievements;
+
     }
 }
