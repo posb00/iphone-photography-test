f1b100878573306c9e989a9ac70b8333ce98350b
alexander <posb00@gmail.com>
2021-10-14 14:11:42 -0400

    achievement controller

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -2,19 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\CommentWritten;
+use App\Models\Comment;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
     public function index(User $user)
     {
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $user->unlocked_achievement,
+            'next_available_achievements' => $user->next_available_achievements,
+            'current_badge' => $user->current_badge,
+            'next_badge' => $user->next_badge,
+            'remaing_to_unlock_next_badge' => $user->remaining_next_badge,
         ]);
+    }
+
+    public function call()
+    {
+        $comment = new Comment();
+        $comment->fill([
+            'body' => 'primer comentario',
+            'user_id' => 1,
+        ]);
+
+        event(new CommentWritten($comment));
+
+    }
+
+    public function callLesson()
+    {
+        $lesson = Lesson::find(4);
+        $user = User::find(1);
+
+        event(new LessonWatched($lesson, $user));
+
     }
 }
